3b6845477eb8b2ef1eb58bb9716219323bddaa12
Snaebjorn Eyjolfsson <snabbie1@gmail.com>
2015-03-19 18:35:50 +0000

    sumList (2a) accepted on mooshak

---
+++ Scheme/sumList.rkt
@@ -0,0 +1,7 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname sumList) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ())))
+(define (sumList lst)
+  (if (null? lst) 0
+      (+ (car lst) (sumList (cdr lst))))
+)
